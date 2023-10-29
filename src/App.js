9d35c308a5416d55eb900f5c0dec47413809b021
Sabri Çakır <sabri.cakir@lab2023.com>
2023-10-29 18:09:09 +0300

    Convert app function to app class

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Navigation from './Navigation';
 import Category from './Category';
 import Product from './Product';
 import { Col, Container, Row } from 'reactstrap';
 
-function App() {
-  let categoryInfo = { title: "Category Component", desc: "Category Description" };
-  let productInfo = { title: "Product Component", desc: "Product Description" };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navigation />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <Category info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <Product info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div >
-  );
+export default class App extends Component {
+  render() {
+    let categoryInfo = { title: "Category Component", desc: "Category Description" };
+    let productInfo = { title: "Product Component", desc: "Product Description" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navigation />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <Category info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <Product info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div >
+    );
+  }
 }
-
-export default App;
