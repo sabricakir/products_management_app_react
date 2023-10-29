5621e0c79349ed2db610ebab4f61b888e86c82c2
Sabri Çakır <sabri.cakir@lab2023.com>
2023-10-29 16:54:25 +0300

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Navigation from './Navigation';
 import Category from './Category';
 import Product from './Product';
+import { Alert } from 'reactstrap';
 
 function App() {
   return (
     <div>
+      <Alert color="danger">
+        This is a primary alert — check it out!
+      </Alert>
+
       <h1>Hello World From Base Component</h1>
       <Navigation />
       <Category />
