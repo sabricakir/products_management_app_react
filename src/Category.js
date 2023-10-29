ed501d0d291e9ef0d294feb70965520043e29c5d
Sabri Çakır <sabri.cakir@lab2023.com>
2023-10-29 18:05:04 +0300

    Add onclick event to list items

--- src/Category.js
+++ src/Category.js
@@ -9,9 +9,15 @@ export default class Category extends Component {
         { id: 1, name: "Beverages" },
         { id: 2, name: "Condiments" },
         { id: 3, name: "Confections" }
-      ]
+      ],
+      currentCategory: ""
     }
   }
+
+  updateCategory = (categoryName) => {
+    this.setState({ currentCategory: categoryName });
+  }
+
   render() {
     return (
       <div className='text-start'>
@@ -19,9 +25,14 @@ export default class Category extends Component {
         <p> {this.props.info.desc} </p>
         <ListGroup>
           {this.state.categories.map(category => (
-            <ListGroupItem key={category.id}> {category.name} </ListGroupItem>
+            <ListGroupItem
+              key={category.id}
+              onClick={() => this.updateCategory(category.name)}>
+              {category.name}
+            </ListGroupItem>
           ))}
         </ListGroup>
+        <p className='text-success'> Selected Category: {this.state.currentCategory} </p>
       </div>
     )
   }
